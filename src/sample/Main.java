0af1aa9770be94d1e1c97ada991483835d96b04f
Stan Verbakel <sjjverba@users.noreply.github.com>
2018-11-07 16:03:30 +0100

    db driver

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+
+import java.sql.DriverManager;
 
 public class Main extends Application {
 
@@ -14,10 +17,29 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        dbTest();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void dbTest()
+    {
+        String DB_URL = "jdbc:mysql://databases.aii.avans.nl:3306/";
+        String SETTINGS = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+        String USER = "sjjverba_db2";
+        String PASS = "Ab12345";
+
+        try{
+            var connection = DriverManager.getConnection(DB_URL+SETTINGS,USER,PASS);
+            new Alert(Alert.AlertType.CONFIRMATION, "db connection succes").showAndWait();
+        }
+        catch(Exception e)
+        {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).showAndWait();
+        }
     }
 }
