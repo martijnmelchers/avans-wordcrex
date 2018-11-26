808bf4ec524fca6fd187f1bb34049c7ad27bb3f5
MartijnMelchers <martijn@melchers.nu>
2018-11-26 14:18:57 +0100

    Added Account class

---
+++ src/model/Account.java
@@ -0,0 +1,19 @@
+package model;
+
+import model.database.annotations.Column;
+import model.database.annotations.PrimaryKey;
+import model.database.annotations.Table;
+
+@Table(name = "account")
+public class Account {
+
+    @Column
+    @PrimaryKey
+    private String username;
+    @Column
+    private String password;
+
+    public Account() {
+
+    }
+}
