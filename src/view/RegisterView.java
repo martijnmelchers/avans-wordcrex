19db9c4d534dbd8025017759a3ede6ca2e41a17c
“Stan <sjjverba@avans.nl>
2018-11-26 13:55:29 +0100

    Login ui

--- src/view/RegisterView.java
+++ src/view/RegisterView.java
@@ -1,22 +1,5 @@
 package view;
 
-import controller.RegisterController;
-
-public class RegisterView extends View
+public class RegisterView
 {
-
-    private RegisterController registerController;
-
-    public void backClicked()
-    {
-        registerController = this.getController();
-        registerController.navigate("LoginView.fxml", 350, 550);
-    }
-
-    public void registerClicked()
-    {
-        registerController = this.getController();
-        registerController.navigate("MainView.fxml");
-    }
-
 }
