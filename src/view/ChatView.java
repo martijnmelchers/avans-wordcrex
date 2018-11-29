b641de20687b1cbdb5a0f3bc497b5435d4a2909e
JoostAdema <ademajoost@hotmail.com>
2018-11-29 20:17:50 +0100

    added some frontend functionality

--- src/view/ChatView.java
+++ src/view/ChatView.java
@@ -1,9 +1,46 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+import java.util.ArrayList;
+
 public class ChatView extends View{
+    private ArrayList<String> messages = new ArrayList<>();
 
-    public ChatView() {
+    @FXML
+    private Pane messagesVbox;
 
+    @FXML
+    private TextField messageField;
+
+    // temporary function for displaying messages
+    public void displayMessages() {
+        messagesVbox.getChildren().clear();
+
+        for(String message : messages){
+            Text displayMessage = new Text();
+            displayMessage.setText(message);
+            messagesVbox.getChildren().add(displayMessage);
+        }
     }
 
+    // temporary function to test display
+    public void sendMessage() {
+        String message = messageField.getText();
+
+        if(!message.isEmpty()){
+            messages.add(message);
+            messageField.clear();
+        }
+
+        displayMessages();
+    }
+
+    // when enter key is pressed send the message
+    public void onEnter(javafx.event.ActionEvent actionEvent) {
+        sendMessage();
+    }
 }
