46349b09dcac040e21d66c918e3671bd9fde1ce2
Dirk <dirkvde1999@hotmail.com>
2018-11-29 15:44:54 +0100

    Checks

--- src/controller/AccountController.java
+++ src/controller/AccountController.java
@@ -15,22 +15,36 @@ public class AccountController extends Controller
         return  password.equals(confirmationPassword);
     }
 
-    public void registerUser(String username, String password,String confirmationPassword)
+    public void registerUser(String username, String password, String confirmationPassword)
     {
         RegisterView registerView = getViewCasted();
+        if (username.isEmpty() || password.isEmpty() || confirmationPassword.isEmpty())
+        {
+            registerView.showError("Vul alle velden in");
+            return;
+        }
+
         if(!checkPasswords(password,confirmationPassword ))
         {
             registerView.showError("De wachtwoorden komen niet overeen");
+            return;
         }
         //TODO: register user
     }
 
-    public void checkUserCredentials(String username ,String password)
+    public void checkUserCredentials(String username, String password)
     {
         LoginView loginView = getViewCasted();
+        if (username.isEmpty() || password.isEmpty())
+        {
+            loginView.showError("Vul alle velden in");
+            return;
+        }
+
         if(true)
         {
             loginView.showError("Inloggegevens onjuist");
+            return;
         }
         // TODO: check user credentials in model
     }
