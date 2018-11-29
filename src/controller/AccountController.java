d312520c3365196ceb003dd3978b22559f6c6730
Stan Verbakel <sjjverba@users.noreply.github.com>
2018-11-29 14:26:48 +0100

    account controller functions

--- src/controller/AccountController.java
+++ src/controller/AccountController.java
@@ -2,11 +2,19 @@ package controller;
 
 public class AccountController extends Controller
 {
-
     public AccountController()
     {
 
     }
 
+    public boolean registerUser(String username, String password)
+    {
+        return false;
+    }
+
+    public boolean checkPassword(String username,String password)
+    {
+        return false;
+    }
 
 }
