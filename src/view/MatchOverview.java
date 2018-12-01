d8c8a20ac28c113a2e16871ba0692a783348b54c
Huseyin Caliskan <huseyin.32@live.nl>
2018-12-01 12:20:37 +0100

    Working

---
+++ src/view/MatchOverview.java
@@ -0,0 +1,32 @@
+package view;
+
+import controller.MatchOverviewController;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+
+public class MatchOverview extends View
+{
+    private MatchOverviewController controller;
+
+    @FXML
+    private ScrollPane m_matchScrollPane;
+
+    public MatchOverview()
+    {
+    }
+
+    public void start()
+    {
+        controller = this.getController(MatchOverviewController.class);
+
+        VBox root = new VBox();
+        root.getChildren().addAll(new Button("test"), new Button("test2"));
+
+
+        m_matchScrollPane.setContent(root);
+    }
+}
