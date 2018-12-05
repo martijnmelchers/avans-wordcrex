6b143c651adde9084d030da6c1643d1ea4842257
Storm <stormwitziers123@gmail.com>
2018-12-05 11:20:12 +0100

    Updated dock

---
+++ src/view/DockView.java
@@ -0,0 +1,12 @@
+package view;
+
+import controller.Controller;
+import controller.GameController;
+
+public class DockView extends View
+{
+    private Controller _controller;
+    /*public DockView(){
+        _controller = getController(GameController.class);
+    }*/
+}
