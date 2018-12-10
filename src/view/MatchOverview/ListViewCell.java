f663823a38277698cd9dd01b45c83e67d05ee2b2
Sascha Mendel <saschamendel2016@gmail.com>
2018-12-10 16:50:48 +0100

    Structure change

---
+++ src/view/MatchOverview/ListViewCell.java
@@ -0,0 +1,16 @@
+package view.MatchOverview;
+
+import javafx.scene.control.ListCell;
+
+public class ListViewCell extends ListCell<String> {
+    @Override
+    public void updateItem(String string, boolean empty)
+    {
+        super.updateItem(string,empty);
+        if(string != null)
+        {
+            MatchView view = new MatchView();
+            setGraphic(view.getAnchor());
+        }
+    }
+}
--- src/view/MatchOverview/MatchOverview.java
+++ src/view/MatchOverview/MatchOverview.java
@@ -1,4 +1,26 @@
 package view.MatchOverview;
 
-public class MatchOverview {
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import view.View;
+
+public class MatchOverview extends View {
+
+
+    @FXML
+    private ListView gameListview;
+    public MatchOverview(){
+
+    }
+
+    public void loadFinished(){
+        gameListview.setCellFactory(new Callback<ListView, ListCell>() {
+            @Override
+            public ListCell call(ListView param) {
+                return new ListViewCell();
+            }
+        });
+    }
 }
