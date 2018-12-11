d2c734503f0fab5567528d07c2b29265132f4d5a
jesperkool <ddfschol@avans.nl>
2018-12-11 15:48:11 +0100

    beter

---
+++ src/model/matchfixer/Matchfixer.java
@@ -0,0 +1,11 @@
+package model.matchfixer;
+
+import model.database.DocumentSession;
+
+public class Matchfixer {
+    public boolean invitePlayer(String Player){
+        DocumentSession.getDatabase().update();
+
+        return false;
+    }
+}
