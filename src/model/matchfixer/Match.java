57d888d031d965b37532cefcb453c11f96a212b6
jesperkool <ddfschol@avans.nl>
2018-12-11 16:15:02 +0100

    beterer

---
+++ src/model/matchfixer/Match.java
@@ -0,0 +1,34 @@
+package model.matchfixer;
+
+import model.database.annotations.*;
+
+@Table("game")
+public class Match {
+    @Column
+    @PrimaryKey
+    @AutoIncrement
+    private Integer game_id;
+    @Column
+    private String game_state;
+    @Column
+    private String letterset_code;
+    @Column
+    private String username_player1;
+    @Column
+    private String username_player2;
+    @Column
+    private String answer_player2;
+    @Column
+    @Nullable
+    private String username_winner;
+
+    public Match(String game_state, String letterset_code, String username_player1, String username_player2, String answer_player2) {
+        this.game_id = null;
+        this.game_state = game_state;
+        this.letterset_code = letterset_code;
+        this.username_player1 = username_player1;
+        this.username_player2 = username_player2;
+        this.answer_player2 = answer_player2;
+    }
+
+}
--- src/model/matchfixer/Matchfixer.java
+++ src/model/matchfixer/Matchfixer.java
@@ -1,11 +1,33 @@
 package model.matchfixer;
 
+import model.GameSession;
 import model.database.DocumentSession;
+import model.database.services.Database;
+import model.helper.Log;
+import model.tables.GameState;
+
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class Matchfixer {
+
+    Database dB;
+
+    public Matchfixer(){
+        try {
+            dB = DocumentSession.getDatabase();
+        } catch (SQLException e) {
+            Log.error(e);
+        }
+    }
+
     public boolean invitePlayer(String Player){
-        DocumentSession.getDatabase().update();
+        dB.update(new Match("request","NL", GameSession.getUsername(),));
 
         return false;
+    }
+    public List<String> searchPlayers(String name){
+
     }
 }
