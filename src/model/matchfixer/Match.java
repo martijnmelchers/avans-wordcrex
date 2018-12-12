acbaa5fd57f3524001e6564f609371f96062634f
jesperkool <ddfschol@avans.nl>
2018-12-12 16:16:20 +0100

    thdthd

--- src/model/matchfixer/Match.java
+++ src/model/matchfixer/Match.java
@@ -27,13 +27,13 @@ public class Match {
     }
 
     public Match(String game_state, String letterset_code, String username_player1, String username_player2, String answer_player2) {
-        this.game_id = null;
+        //this.game_id = null;
         this.game_state = game_state;
         this.letterset_code = letterset_code;
         this.username_player1 = username_player1;
         this.username_player2 = username_player2;
         this.answer_player2 = answer_player2;
-        this.username_winner = null;
+        //this.username_winner = null;
     }
     public boolean Participates(String name) {
 
